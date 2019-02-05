d926d7b530ddfe4ee5cd692459e964b72500ab66
fetuolemoana <fetu.tamapeau@gmail.com>
2019-02-06 11:56:07 +1300

    Setup basic data structure

---
+++ client/data/muagagana.js
@@ -0,0 +1,5 @@
+const muagagana = {
+  type: ["upu fa'aaloalo: expressions of respect and courtesy", "upu fa'amaulalo: respect and courtesy in the form of self-abasement", "upu vivi'i: laudatory and complimentary remarks", "upu fiafia: expressions of joy and contentment", "upu alofa: expressions of love, compassion, and sympathy", "upu fa'anoanoa: expressions of repentance and remorse", "upu fa'aulaula: raillery, ridicule, jests", "upu faifai: offensive expressions, insults", "upu fa'aalualu: encouragement, persuasion", "upu fa'amafanafana: comforting, consolatory expressions", "upu taofiofi: warning, exhortation, appeasement", "upu fa'afiti: denial, refusal"]
+}
+
+export default muagagana
---
+++ client/data/proverbs.js
@@ -0,0 +1,247 @@
+const proverbs = [ 
+  {
+    id: 1,
+    proverb: "Ia a le puga, a le 'ana, or la a le puga niisi, a le 'ana niisi.", 
+    translation: "Some parts are polished with puga, others with 'ana.",
+    description: 'x', 
+    type: 'x',
+    topic: 'x'
+  },
+  {
+    id: 2,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 3,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 4,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 5,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 6,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 7,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 8,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 9,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 10,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 11,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 12,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 13,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 14,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 15,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 16,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 17,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 18,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 19,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 20,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 21,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 22,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 23,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 24,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 25,
+    proverb: 'x', 
+    description: 'x', 
+    type: 
+    'x'
+  },
+  {
+    id: 26,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 27,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 28,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 29,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 30,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 31,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 32,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 33,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 34,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 35,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 36,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 37,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 38,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 39,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+  {
+    id: 40,
+    proverb: 'x', 
+    description: 'x', 
+    type: 'x'
+  },
+]
+
+export default proverbs

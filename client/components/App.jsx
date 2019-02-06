f184e819e0158c2e9121fd49ace4d050d3dba24d
fetuolemoana <fetu.tamapeau@gmail.com>
2019-02-06 16:33:59 +1300

    Import components setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { Route } from react-router-dom
+import { Route } from 'react-router-dom'
+
+import Nav from './Nav'
+import Header from './Header'
+import Home from './Home'
+import Topics from './Topics'
+import Muagagana from './Muagagana'
+import Proverb from './Proverb'
+
 
 const App = () => {
   return (

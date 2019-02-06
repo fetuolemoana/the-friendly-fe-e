98f884325e426605c6f70f7b52405685aede04b6
fetuolemoana <fetu.tamapeau@gmail.com>
2019-02-06 19:05:49 +1300

    Add app route paths

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,7 +11,16 @@ import Proverb from './Proverb'
 
 const App = () => {
   return (
-    <h1>the friendly fe'e</h1>
+    <React.Fragment>
+      <Header />
+      <Nav />
+
+      <Route exact path = '/' component = {Home} />
+      <Route path = '/topics' component = {Topics} />
+      <Route path = '/muagagana' component = {Muagagana} />
+      <Route path = '/proverb/:id' component = {Proverb} />
+
+    </React.Fragment>
   )
 }
 

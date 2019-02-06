8d1516bea696757332d1473f5c547ebddf5359a6
fetuolemoana <fetu.tamapeau@gmail.com>
2019-02-07 08:20:46 +1300

    Add purpose list to component

--- client/components/PurposeList.jsx
+++ client/components/PurposeList.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import filters from '../data/filters';
 
 const PurposeList = () => {
   return (
     <React.Fragment>
       <h1>the friendly fe'e purpose list</h1>
+      <ul>
+        {filters.purpose.map(function(pur) {
+          return <li>{pur}</li>
+        })
+        }
+      </ul>
     </React.Fragment>
   )
 }
